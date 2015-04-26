33cc48701c2568056629385c150cba4992ea542c
Stuart Lee <stuart.lee@drpgroup.com>
2015-04-26 22:26:58 +0100

    added mocha/chai

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('chai').should();
+var PIXI = require('pixi.js');
+var pixiTiled = require('../index.js');
+
+//
+//foo.should.be.a('string');
+//foo.should.equal('bar');
+//foo.should.have.length(3);
+//beverages.should.have.property('tea').with.length(3);
